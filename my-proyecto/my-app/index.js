39c5863652a7e1f177fb3d5eea3f65555de7bde9
= <=>
2022-06-01 21:02:35 -0300

    proyecto

---
+++ my-proyecto/my-app/index.js
@@ -0,0 +1,43 @@
+/*
+class Usuario {
+    constructor(firstName, lastName, books, pets) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.books = books;
+        this.pets = pets
+    }
+    getFullName() {
+        return this.firstName + " " + this.lastName
+    }
+    addPet(pet) {
+        this.pets.push(pet);
+    }
+    countPets() {
+        return this.pets.length
+    }
+    addBook(name, author) {
+        this.books.push({ name: name, author: author });
+    }
+    getBooksName() {
+        let names = []
+        this.books.forEach(element => {
+            names.push(element.name)
+        });
+        return names
+    }
+}
+let usuario = new Usuario("Blanca", "Robles", [], [])
+console.log(usuario.getFullName());
+usuario.addPet("Marvel")
+usuario.addPet("Mü")
+usuario.addPet("Simon")
+usuario.addPet("Cala")
+usuario.addPet("Rocco")
+usuario.addBook("El desorden que dejas", "Carla Montero")
+usuario.addBook("El lobo estepario", "Hermann Hesse")
+usuario.addBook("Donde el corazon te lleve", "Susana Tamaro")
+usuario.addBook("El ingles de los güesos", "Benito lynch")
+usuario.addBook("Arboles del abalorio", "Hermann Hesse")
+usuario.addBook("El bar del infierno", "Alejandro Dolina")
+console.log(`cantidad de animales: ${usuario.countPets()}`)
+console.log(`Los nombres de los libros: ${usuario.getBooksName()}`)*/
